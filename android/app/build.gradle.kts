e5a6e48f9456ab69c7fd3aa3ad0734b5550d988b
shashankmp2004 <shashankmp2004@gmail.com>
2025-09-27 16:16:16 +0530

    fixed working

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.edgesync"
-        minSdk = flutter.minSdkVersion
+        minSdkVersion(26)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
